a11cd74b2c88e0675110e51d2bc2427b37938310
Kevin Chen <kchen@OLM-LOANER3060.local>
2015-05-21 13:22:26 -0400

    added rscript to make heatmaps for generic tab delimited file

---
+++ make_heat.r
@@ -0,0 +1,15 @@
+msg <- "meowmeowmeow"
+library(qplots)
+
+message("Choose file for heatmap (tab delimited)")
+raw <- read.csv(file.choose(),sep="\t")
+
+rnames = raw[,1]
+numraw = raw[-1]
+
+mata = data.matrix(numraw)
+
+rownames(mata) = rnames
+
+other_palette <- colorRampPalette (c("red2","yellow2","green2"))(n=100)
+heatmap.2 (mata,Rowv=NA,Colv=NA,scale="none",margins=c(18,6),col=other_palette,cexRow=0.5,density.info="none",trace="none")
